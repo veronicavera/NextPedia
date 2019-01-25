33422fa7090400d3f9c0778161e53e54893f2114
Shawn Ard <shawnduncanard@gmail.com>
2019-01-25 15:09:06 -0600

    base files for landing and login via firebase

---
+++ client/src/components/LoginLanding/Account/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { AuthUserContext } from '../Session';
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+import { withAuthorization } from '../Session';
+
+const AccountPage = () => (
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
+);
+
+const authCondition = authUser => !!authUser;
+
+export default withAuthorization(authCondition)(AccountPage);
