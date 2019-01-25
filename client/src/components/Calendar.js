8d504967dd967a49e6dd14ed24a5b5bdf89af152
Ry <ry.e.desoto@gmail.com>
2019-01-24 19:57:08 -0600

    Added 6-month window for choosing flights
    We are only allowing flights within a certain amount of time -- added logic for the timeframe

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Calendar from "react-calendar";
+import moment from "moment";
 
 class MyCalendar extends Component {
   constructor(props) {
@@ -9,15 +10,39 @@ class MyCalendar extends Component {
     };
   }
 
+  // note from ry, 2019-01-24 -- the "-1" in the month section is because javascript starts counting
+  // months (and ONLY months) from month 0
+  // month 0 = january; month 11 = december
+
+  // in order to get from Moment months (counted 1-12) to js months (0-11), moment month - 1 = js month
+
   render() {
+    const monthOffset = 6;
+    const year = parseInt(
+      moment()
+        .add(monthOffset, "months")
+        .format("YYYY")
+    );
+    const month =
+      parseInt(
+        moment()
+          .add(monthOffset, "months")
+          .format("MM")
+      ) - 1;
+    const day = parseInt(
+      moment()
+        .add(monthOffset, "months")
+        .format("DD")
+    );
+
     return (
       <div>
         <Calendar
-          onChange={this.props.onChange}
           value={this.state.date}
+          onChange={this.props.onChange}
           view={"month"}
           minDate={new Date()}
-          maxDate={new Date(2019, 2, 1)}
+          maxDate={new Date(year, month, day)}
         />
       </div>
     );
