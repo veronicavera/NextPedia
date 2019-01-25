df3a8acb2507d27734233b738c5bc8fd403cd6fe
Kaleb Kougl <kalebkougl@gmail.com>
2019-01-25 11:24:56 -0600

    Basic flightFinder component

---
+++ client/src/components/pages/FlightFinder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class FlightFinder extends Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div>
+        Hello this is the flight finder page.
+      </div>
+    );
+  }
+}
+
+export default FlightFinder;
