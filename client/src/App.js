1aeb835d96656f281525370ed56117a7e2eab695
Kaleb Kougl <kalebkougl@gmail.com>
2019-01-27 11:15:31 -0600

    fixing file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
-import User from "./components/User"
+import User from "./components/pages/User"
 import FlightFinder from './components/pages/FlightFinder';
 
 class App extends Component {
