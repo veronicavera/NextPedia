c16b5d693ed6ad9af6f4f7e2824d3c105b2f019e
Kaleb Kougl <kalebkougl@gmail.com>
2019-01-28 16:32:15 -0600

    adding colon to give a little better lookie look

--- client/src/components/FlightSearchForm.js
+++ client/src/components/FlightSearchForm.js
@@ -36,20 +36,20 @@ class FlightSearchForm extends Component {
           <div className="card-body" style={{ height: '90vh' }}>
             <form id="flight-search-form" style={styles.form}>
               <label>
-                Origin
+                Origin:
           <input name="origin" type="text" value={this.state.origin} onChange={this.handleInputChange} />
               </label>
               <label>
-                Leaving Date
+                Leaving Date:
           <input name="departureDate" type="date" value={this.state.departureDate} onChange={this.handleInputChange} />
               </label>
               <label>
-                Destination
+                Destination:
           <input name="destination" type="text" value={this.state.destination} onChange={this.handleInputChange} />
               </label>
               <label>
-                Length Of Stay
-          <input name="lengthOfStay" type="number" value={this.state.lengthOfStay} onChange={this.handleInputChange} />
+                Length Of Stay:
+                <input name="lengthOfStay" type="number" value={this.state.lengthOfStay} onChange={this.handleInputChange} />
               </label>
               <Calendar />
               <input type="submit" value="Submit" />
