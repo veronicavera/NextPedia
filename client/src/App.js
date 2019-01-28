7353c5f97543ce80310b612297c9d0250c8b3c65
Ry <ry.e.desoto@gmail.com>
2019-01-28 13:11:07 -0600

    TripDetails got eaten - putting htis back into the import statement

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import {
+  FlightFinder,
+  TripDetails,
+  Landing,
+  User,
+  FAQ,
+  About,
+  Contact
+} from "./components/pages";
+import { withAuthentication } from "./components/pages/Session";
+import SignUpPage from "./components/pages/SignUp";
 
-import { FlightFinder, Landing, User, FAQ, About, Contact } from './components/pages';
-import { withAuthentication } from './components/pages/Session'
-import SignUpPage from './components/pages/SignUp';
-
-import SignInPage from './components/pages/SignIn';
-import Navigation from './components/pages/Navigation';
-import HomePage from './components/pages/Home';
-import { MenuBar, Footer } from './components';
-import dotenv from 'dotenv';
+import SignInPage from "./components/pages/SignIn";
+import Navigation from "./components/pages/Navigation";
+import HomePage from "./components/pages/Home";
+import { MenuBar, Footer } from "./components";
+import dotenv from "dotenv";
 dotenv.config();
-
 
 class App extends Component {
   render() {
@@ -22,19 +28,17 @@ class App extends Component {
         <>
           <Navigation />
           <Switch>
-
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/home' component={HomePage} />
-            <Route exact path='/test' component={Home} />
-            <Route exact path='/user' component={User} />
-            <Route exact path='/flightFinder' component={FlightFinder} />
-            <Route exact path='/FAQ' component={FAQ} />
-            <Route exact path='/About' component={About} />
-            <Route exact path='/Contact' component={Contact} />
-            <Route exact path='/Signin' component={SignInPage} />
-            <Route exact path='/signup' component={SignUpPage} />
-            <Route exact path="/tripdetails" component={TripDetails} />      
-
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/home" component={HomePage} />
+            <Route exact path="/test" component={Home} />
+            <Route exact path="/user" component={User} />
+            <Route exact path="/flightFinder" component={FlightFinder} />
+            <Route exact path="/FAQ" component={FAQ} />
+            <Route exact path="/About" component={About} />
+            <Route exact path="/Contact" component={Contact} />
+            <Route exact path="/Signin" component={SignInPage} />
+            <Route exact path="/signup" component={SignUpPage} />
+            <Route exact path="/tripdetails" component={TripDetails} />
           </Switch>
           <Footer />
         </>
