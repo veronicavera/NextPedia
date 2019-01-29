7950d9581578f89cc4d1c1ca9862ee2cd8e59bf0
John <jjge732@gmail.com>
2019-01-28 19:12:01 -0600

    made more mock data returned on call and made data more realistic

--- controller/flightsController.js
+++ controller/flightsController.js
@@ -18,27 +18,52 @@ module.exports = {
                 } else if (randomNumber === 0){ 
                     return 'B6';
                 } else {
-                    return 'How?'
+                    return 'Error finding airline'
                 };
             }
 
             const populateFlightData = () => {
-                let time = moment('2019-4-4 12:30 pm', 'YYYY-MM_DD hh:mm a').format();
-                let hours = flightInformation[0].time.split('h')[0];
+                let date = req.body.date || '2019-4-4' 
+                let time1 = moment(date + ' 07:30 am', 'YYYY-MM_DD hh:mm a').format();
+                let time2 = moment(date + ' 01:30 pm', 'YYYY-MM_DD hh:mm a').format();
+                let time3 = moment(date + ' 05:30 pm', 'YYYY-MM_DD hh:mm a').format();
+
+                let hours = parseInt(flightInformation[0].time.split('h')[0]) + parseInt(timeOffset);
                 let minutes = parseInt(flightInformation[0].time.split(' ')[1].replace('min')) + (60 * hours);
-                
+
                 let flightData = [
                     {
                         airlineName: getAirlineCode(),
-                        departureTime: time.replace(moment('2019-4-4 12:30 pm', 'YYYY-MM_DD hh:mm a').format('Z'), ''),
-                        arrivalTime: moment(time).add(minutes, 'minutes').format().replace(moment('2019-4-4 12:30 pm', 'YYYY-MM_DD hh:mm a').format('Z'), ''),
+                        departureTime: time1.replace(moment(date + ' 07:30 pm', 'YYYY-MM-DD hh:mm a').format('Z'), ''),
+                        arrivalTime: moment(time1).add(minutes, 'minutes').format().replace(moment(date + ' 07:30 pm', 'YYYY-MM_DD hh:mm a').format('Z'), ''),
+                        departureAirport: flightInformation[0].start,
+                        arrivalAirport: flightInformation[0].dest,
+                        flightFare: Math.floor(flightInformation[0].distance / 10)
+                    },
+                    {
+                        airlineName: getAirlineCode(),
+                        departureTime: time2.replace(moment(date + ' 01:00 pm', 'YYYY-MM-DD hh:mm a').format('Z'), ''),
+                        arrivalTime: moment(time2).add(minutes, 'minutes').format().replace(moment(date + ' 01:00 pm', 'YYYY-MM_DD hh:mm a').format('Z'), ''),
+                        departureAirport: flightInformation[0].start,
+                        arrivalAirport: flightInformation[0].dest,
+                        flightFare: Math.floor(flightInformation[0].distance / 10)
+                    },
+                    {
+                        airlineName: getAirlineCode(),
+                        departureTime: time3.replace(moment(date + ' 05:30 pm', 'YYYY-MM-DD hh:mm a').format('Z'), ''),
+                        arrivalTime: moment(time3).add(minutes, 'minutes').format().replace(moment(date + ' 05:30 pm', 'YYYY-MM_DD hh:mm a').format('Z'), ''),
                         departureAirport: flightInformation[0].start,
                         arrivalAirport: flightInformation[0].dest,
                         flightFare: Math.floor(flightInformation[0].distance / 10)
                     }
+                    
                 ]
-                res.json(flightData);
-                return flightData;
+                if (route.data.steps[0].distance.flight[0].start !== startAirport || route.data.steps[0].distance.flight[0].dest !== endAirport) {
+                    res.send('No flights available between the selected airports');
+                } else {
+                    res.json(flightData);
+                    return flightData;
+                }
             }
             let startAirport = req.body.startAirport, endAirport = req.body.endAirport;
             const route = await axios({
@@ -49,11 +74,12 @@ module.exports = {
                         {"t": startAirport},
                         {"t": endAirport}
                     ])
+
                 },
                 headers: { "X-RapidAPI-Key": process.env.FLIGHTS_API_KEY}
             })
-
             const flightInformation = route.data.steps[0].distance.flight
+            const timeOffset = route.data.steps[0].time;
             return populateFlightData();
         }
         promise();
