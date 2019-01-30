797d75a74c47baf6ea5a503264484695ebf168b4
Ry <ry.e.desoto@gmail.com>
2019-01-30 00:29:49 -0600

    adding API to find airport information

---
+++ controller/connectionSQL.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+
+    // Your port; if not 3306
+    port: 8889,
+
+    // Your username
+    user: 'root',
+
+    // Your password
+    password: 'root',
+    database: 'nextpedia_db'
+  });
+}
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId + '\n');
+});
+
+module.exports = connection;
---
+++ controller/sqlController.js
@@ -0,0 +1,20 @@
+const connection = require('./connectionSQL');
+
+module.exports = {
+  lookUpAirport: (req, res) => {
+    console.log(req.params.iata);
+    // connection
+    //   .query(`SELECT * FROM AIRPORTS WHERE IATA = "${req.params.iata}";`)
+    //   .then(airportdata => res.json(airportdata))
+    //   .catch(err => res.status(422).json(err));
+
+    connection.query(
+      `SELECT * FROM AIRPORTS WHERE IATA = "${req.params.iata}";`,
+      function(err, data) {
+        if (err) throw err;
+        console.log(data);
+        res.json(data);
+      }
+    );
+  }
+};
