24866ecca2fc32b66ba938a340359a210f847b01
Alex <AlexGreen2015@icloud.com>
2019-01-30 15:06:20 -0600

    user to trip details

--- client/src/components/utils/userTripsGet.js
+++ client/src/components/utils/userTripsGet.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from "axios";
-
-
+import { withRouter } from 'react-router-dom';
 class FetchData extends React.Component {   
  
     state={
@@ -9,19 +8,24 @@ class FetchData extends React.Component {
         tripId:[]
     }
     componentDidMount() {
-     axios.get(`http://localhost:5000/api/trips/flightAficionado@ymail.com`)
+     axios.get(`http://localhost:5000/api/trips/flightAficionado@ymail.com`) //should be changed on `/api/trips/`+localStorage.user when deployed on heroku and real data is gonna be used
        .then(res => {
+           
         res.data[0].trips.map(trip=>{
             var x = this.state.tripName;
             var y= this.state.tripId;
             x.push(trip.tripName);
             y.push(trip._id);
+            console.log(y)
             this.setState({tripName:x});
             this.setState({tripId:y});
             });
        });
     }
-    
+    _handleButtonClick = (event) => {
+        this.props.history.push("/tripdetails/"+event.target.getAttribute('data-id'))
+      }
+      
 render(){
     const thStyle = {
         border: "1px solid gray",
@@ -29,14 +33,14 @@ render(){
         textAlign: "center"
     }
 
-
+      
     return(
     <> 
     {this.state.tripName.map((item,i)=>{
         
         return <tr>   
               <td style={thStyle}>{item}</td>
-              <td style={thStyle} ><button className="btn btn-info" data-id={this.state.tripId[i]}>Manage trip</button></td>
+              <td style={thStyle} ><button className="btn btn-info" data-id={this.state.tripId[i]} onClick={this._handleButtonClick}>Manage trip</button></td>
               </tr>
             
     })}
@@ -45,4 +49,4 @@ render(){
     }    
 }  
 
-export default FetchData;
+export default withRouter(FetchData);
