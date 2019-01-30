ec23d140f284e29a230b051c01beaf83773aedee
John <jjge732@gmail.com>
2019-01-29 22:01:45 -0600

    put the form into a grid

--- client/src/components/pages/Contact/index.js
+++ client/src/components/pages/Contact/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import './index.css';
+import './index.css'
+
 class Contact extends Component {
   state = {
     body: '',
     subject: ''
   }
+  
   handleInputChange = event => {
     let value = event.target.value;
     const name = event.target.name;
@@ -13,27 +15,41 @@ class Contact extends Component {
     });
   };
 
+  handleFormSubmit = event => {
+    event.preventDefault();
+    window.location.assign(`mailto:nextpedia21@gmail.com?subject=${this.state.subject}&body=${this.state.body}`)
+  }
+
   render() {
       return (
-          <>
-              <h1>
-                  Contact Us!
-              </h1>
-              <form action={`mailto:nextpedia21@gmail.com?subject=Please Help Me&body=${this.state.body}`} method='post' enctype='text/plain'>
-                  <h2>
-                      What can we help you with?
-                  </h2>
-                  <textarea
-                      value={this.state.body}
-                      name="body"
-                      type="text"
-                      placeholder="body"
-                      onChange={this.handleInputChange}
-                  />
-                  <input type='reset'/>
-                  <button type='submit'>Submit</button>
-              </form>
-          </>
+        <div>
+            <h1>
+                Contact Us!
+            </h1>
+            <form id='contactForm'>
+                <h2>
+                    What can we help you with?
+                </h2>
+                <textarea
+                    value={this.state.subject}
+                    name="subject"
+                    type="text"
+                    placeholder="Subject"
+                    onChange={this.handleInputChange}
+                />
+                <h2>
+                    Please describe the issue in more depth
+                </h2>
+                <textarea
+                    value={this.state.body}
+                    name="body"
+                    type="text"
+                    placeholder="Body"
+                    onChange={this.handleInputChange}
+                />
+                <button onClick={this.handleFormSubmit} type='submit'>Submit</button>
+            </form>
+        </div>
       );
     }
 }
