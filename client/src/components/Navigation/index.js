f5f640a7bd942dbae9a8d8e11da4713ada29068d
Ry <ry.e.desoto@gmail.com>
2019-01-31 17:43:38 -0600

    aes changes

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -10,73 +10,77 @@ import './navigation.css';
 const btnStyle = {
   lineHeight: 'inherit',
   padding: '0.25rem'
-}
+};
 
 const Navigation = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <nav className='navbar navbar-expand-lg navbar-light bg-light'>
     <>
-
       <AuthUserContext.Consumer>
-        {authUser =>
-          authUser ? <NavigationAuth /> : <NavigationNonAuth />
-        }
+        {authUser => (authUser ? <NavigationAuth /> : <NavigationNonAuth />)}
       </AuthUserContext.Consumer>
     </>
   </nav>
 );
 
 const NavigationAuth = () => (
-  <ul className="navbar-nav links">
-
+  <ul className='navbar-nav links'>
     <li style={btnStyle}>
-      <Link to={ROUTES.LANDING} >Home</Link>
-
+      <Link to={ROUTES.LANDING}>Home</Link>
     </li>
     <li style={btnStyle}>
-      <Link to={ROUTES.ACCOUNT} >Profile</Link>
+      <Link to={ROUTES.ACCOUNT}>Profile</Link>
     </li>
     <li>
       <SignOutButton />
     </li>
   </ul>
 );
-                           
+
 class NavigationNonAuth extends React.Component {
   state = {
     signIn: false
-  }
+  };
   componentDidMount() {
-    console.log(this)
+    console.log(this);
   }
   render() {
     return !this.state.signIn ? (
-      <ul className="navbar-nav links">
+      <ul className='navbar-nav links'>
         <li style={btnStyle}>
           <Link to={ROUTES.LANDING}>Home</Link>
         </li>
         <li style={btnStyle}>
           <Link
             to={ROUTES.SIGN_IN}
-            className="btn btn-outline-primary linkBtn"
-            onClick={() => this.setState({signIn: true})}
-          >Sign In</Link>
+            className='btn btn-outline-primary linkBtn'
+            onClick={() => this.setState({ signIn: true })}
+          >
+            Sign In
+          </Link>
         </li>
       </ul>
     ) : (
-      <ul className="navbar-nav links">
+      <ul className='navbar-nav links'>
         <li style={btnStyle}>
-          <Link to={ROUTES.LANDING} onClick={() => this.setState({signIn: false})}>Home</Link>
+          <Link
+            to={ROUTES.LANDING}
+            onClick={() => this.setState({ signIn: false })}
+          >
+            Home
+          </Link>
         </li>
-        <li style={btnStyle} >
+        <li style={btnStyle}>
           <Link
             to={ROUTES.SIGN_UP}
-            className="btn btn-outline-primary linkBtn"
-            onClick={() => this.setState({signIn: false})}
-          >Sign Up</Link>
+            className='btn btn-outline-primary linkBtn'
+            onClick={() => this.setState({ signIn: false })}
+          >
+            Sign Up
+          </Link>
         </li>
       </ul>
-    ) 
+    );
   }
-};
+}
 
 export default Navigation;
