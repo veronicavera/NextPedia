cc03f6c5e10f3eea3760d5f797ac2bbc232d9e71
John <jjge732@gmail.com>
2019-02-01 10:49:08 -0600

    fixed casing issue

--- controller/sqlController.js
+++ controller/sqlController.js
@@ -9,7 +9,7 @@ module.exports = {
     //   .catch(err => res.status(422).json(err));
 
     connection.query(
-      `SELECT * FROM AIRPORTS WHERE IATA = "${req.params.iata}";`,
+      `SELECT * FROM airports WHERE IATA = "${req.params.iata}";`,
       function(err, data) {
         if (err) throw err;
         // console.log(data);
