2150f2a8feee190dae60929fad4ade95c0896f0b
John <jjge732@gmail.com>
2019-01-31 23:19:39 -0600

    style on user page

--- client/src/components/pages/User/index.js
+++ client/src/components/pages/User/index.js
@@ -8,53 +8,55 @@ import './user.css';
 class User extends Component {
   render() {
     return (
-      <div className='container center'>
-        <div className='user-content'>
-          <div className='row user-greeting'>
-            <AccountPage />
-          </div>
-          <br />
-          <React.Fragment>
-            <div className='user-dashboard'>
-              <br />
-              <div className='user-dashboard-info'>
-                <div className='row'>
-                  <div className='col-md-4'>
-                    <h2 className='center'>Create a new trip</h2>
-                  </div>
-                  <div className='col-md-8'>
-                    <h2 className='center'>Manage your trips</h2>
-                  </div>
-                </div>
+      <div id='backgroundImageContainer'>
+        <div className='container center'>
+          <div className='user-content'>
+            <div className='row user-greeting'>
+              <AccountPage />
+            </div>
+            <br />
+            <React.Fragment>
+              <div className='user-dashboard'>
                 <br />
-                <div className='row'>
-                  <div className='col-md-4'>
-                    <Link to={ROUTES.LANDING} className='btn btn-danger'>
-                      Go To Flight Finder
-                    </Link>
+                <div className='user-dashboard-info'>
+                  <div className='row'>
+                    <div className='col-md-4'>
+                      <h2 className='center'>Create a new trip</h2>
+                    </div>
+                    <div className='col-md-8'>
+                      <h2 className='center'>Manage your trips</h2>
+                    </div>
                   </div>
-                  <div className='col-md-8'>
-                    <table>
-                      {/* <table style={tableStyle}> */}
-                      <thead>
-                        <tr>
-                          <th>
-                            <h2>Trip name</h2>
-                          </th>
-                          <th>
-                            <h2>Manage</h2>
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <FetchData />
-                      </tbody>
-                    </table>
+                  <br />
+                  <div className='row'>
+                    <div className='col-md-4'>
+                      <Link to={ROUTES.LANDING} className='btn btn-danger flightFinderBtn'>
+                        Go To Flight Finder
+                      </Link>
+                    </div>
+                    <div className='col-md-8'>
+                      <table>
+                        {/* <table style={tableStyle}> */}
+                        <thead>
+                          <tr>
+                            <th>
+                              <h2>Trip name</h2>
+                            </th>
+                            <th>
+                              <h2>Manage</h2>
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <FetchData />
+                        </tbody>
+                      </table>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </React.Fragment>
+            </React.Fragment>
+          </div>
         </div>
       </div>
     );
