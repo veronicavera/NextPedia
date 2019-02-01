c10cf41d6147661f3d1af6b8fa0b6cb9bcae6888
Ry <ry.e.desoto@gmail.com>
2019-01-31 22:35:50 -0600

    added some additional styling to user page

--- client/src/components/pages/User/index.js
+++ client/src/components/pages/User/index.js
@@ -1,61 +1,64 @@
 import React, { Component } from 'react';
-import FetchData from "../../utils/userTripsGet.js"
-import { AccountPage } from "../../index"
+import FetchData from '../../utils/userTripsGet.js';
+import { AccountPage } from '../../index';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../../constants/routes';
-import './user.css'
-
-
+import './user.css';
 
 class User extends Component {
-
-    render() {
-        const tableStyle = {
-            width: "100%",
-
-        }
-        const thStyle = {
-            border: "1px solid gray",
-            padding: "20px",
-            textAlign: "center"
-        }
-        return (
-            <div className = 'container center'>
-                 <div className="row"><AccountPage /></div>
-                 <br />
-                <React.Fragment>
-                    <div>
-                        <br />
-                        
-                            <div className="row">
-                                <div className="col-md-6"><h2 className="center">Create a new trip</h2></div>
-                                <div className="col-md-6"><h2 className="center">Manage your trips</h2></div>
-                            </div>
-                            <br />
-                            <div className="row">
-                            <div className="col-md-6">
-                            <Link to={ROUTES.LANDING} className="btn btn-danger">Create a new trip</Link>
-                            </div>
-                                <div className="col-md-6">
-                                    <table style={tableStyle}>
-                                        <thead>
-                                            <tr>
-                                                <th style={thStyle}>Trip name</th>
-                                                <th style={thStyle}>Manage</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <FetchData />
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    
-                </React.Fragment>
+  render() {
+    return (
+      <div className='container center'>
+        <div className='user-content'>
+          <div className='row user-greeting'>
+            <AccountPage />
+          </div>
+          <br />
+          <React.Fragment>
+            <div className='user-dashboard'>
+              <br />
+              <div className='user-dashboard-info'>
+                <div className='row'>
+                  <div className='col-md-4'>
+                    <h2 className='center'>Create a new trip</h2>
+                  </div>
+                  <div className='col-md-8'>
+                    <h2 className='center'>Manage your trips</h2>
+                  </div>
+                </div>
+                <br />
+                <div className='row'>
+                  <div className='col-md-4'>
+                    <Link to={ROUTES.LANDING} className='btn btn-danger'>
+                      Go To Flight Finder
+                    </Link>
+                  </div>
+                  <div className='col-md-8'>
+                    <table>
+                      {/* <table style={tableStyle}> */}
+                      <thead>
+                        <tr>
+                          <th>
+                            <h2>Trip name</h2>
+                          </th>
+                          <th>
+                            <h2>Manage</h2>
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <FetchData />
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
             </div>
-        );
-    }
+          </React.Fragment>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default User;
--- client/src/components/utils/userTripsGet.js
+++ client/src/components/utils/userTripsGet.js
@@ -1,55 +1,68 @@
 import React from 'react';
-import axios from "axios";
+import axios from 'axios';
 import { withRouter } from 'react-router-dom';
-class FetchData extends React.Component {   
- 
-    state={
-        tripName:[],
-        tripId:[]
-    }
+import './userTripsGet.css';
 
-    componentDidMount() {
-        axios.get(`http://localhost:5000/api/trips/` + localStorage.user) //should be changed on `/api/trips/`+localStorage.user when deployed on heroku and real data is gonna be used
+class FetchData extends React.Component {
+  state = {
+    tripName: [],
+    tripId: []
+  };
 
-        .then(res => {
-          if(res.data[0]){
-            res.data[0].trips.map(trip=>{
-                var x = this.state.tripName;
-                var y= this.state.tripId;
-                x.push(trip.tripName);
-                y.push(trip._id);
-                console.log(y)
-                this.setState({tripName:x});
-                this.setState({tripId:y});
-                });
-           }
-        });
-    }
-    _handleButtonClick = (event) => {
-        this.props.history.push("/tripdetails/"+event.target.getAttribute('data-id'))
-      }
-      
-render(){
-    const thStyle = {
-        border: "1px solid gray",
-        padding: "10px",
-        textAlign: "center"
-    }
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/trips/` + localStorage.user) //should be changed on `/api/trips/`+localStorage.user when deployed on heroku and real data is gonna be used
 
-      
-    return(
-    <> 
-    {this.state.tripName.map((item,i)=>{
-        
-        return <tr>   
-              <td style={thStyle}>{item}</td>
-              <td style={thStyle} ><button className="btn btn-info" data-id={this.state.tripId[i]} onClick={this._handleButtonClick}>Manage trip</button></td>
-              </tr>
-            
-    })}
-        
-        </>)
-    }    
-}  
+      .then(res => {
+        if (res.data[0]) {
+          res.data[0].trips.map(trip => {
+            var x = this.state.tripName;
+            var y = this.state.tripId;
+            x.push(trip.tripName);
+            y.push(trip._id);
+            console.log(y);
+            this.setState({ tripName: x });
+            this.setState({ tripId: y });
+          });
+        }
+      });
+  }
+  _handleButtonClick = event => {
+    this.props.history.push(
+      '/tripdetails/' + event.target.getAttribute('data-id')
+    );
+  };
+
+  render() {
+    // const thStyle = {
+    //   border: '1px solid gray',
+    //   padding: '10px',
+    //   textAlign: 'center'
+    // };
+
+    return (
+      <>
+        {this.state.tripName.map((item, i) => {
+          return (
+            <tr>
+              {/* <td style={thStyle}>{item}</td>
+              <td style={thStyle}> */}
+              <td>{item}</td>
+              <td>
+                <button
+                  className='btn btn-info'
+                  data-id={this.state.tripId[i]}
+                  onClick={this._handleButtonClick}
+                >
+                  Manage trip
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </>
+    );
+  }
+}
 
 export default withRouter(FetchData);
